5fe0fbdff5d60e0ea4fc044ee2bb1e006d4a2ac8
Lenka <aruru.goddes@gmail.com>
2024-04-15 02:28:05 +0300

    2.09

---
+++ src/test/java/pro/sky/Homework_2/_stream_API/EmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Homework_2._stream_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
